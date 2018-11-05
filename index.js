3ec0f90c52dde0922bb8103b7f0509430d368288
Jonny15t <brokensyn779@gmail.com>
2018-11-05 15:50:22 -0800

    begin gallery formatting

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const express      = require('express'),
       app          = express(),
       PORT         = 8081,
-      errorHandler = require('./handlers/errors');
+      errorHandler = require('./handlers/errors'),
+      coolImages   = require('cool-images'),
+      randomPuppy  = require('random-puppy');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
 
 // Index Route
-app.get('/', (req, res, next) => {
-  res.render('landing');
+app.get('/', async (req, res, next) => {
+  // const images = coolImages.many(400, 400, 30);
+
+  const getPuppies = async () => {
+    const puppies = []
+
+    for(i = 0; i < 30; i++) {
+      const puppy = await randomPuppy();
+      puppies.push(puppy);
+    }
+    
+    return puppies
+  }
+
+  const puppyList = await getPuppies();
+
+  res.render('landing', {
+    images: puppyList
+  });
 });
 
 // 404 Error Generator
